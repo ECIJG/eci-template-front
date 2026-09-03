c41efba83a6540d92679686e0bba13c297fae6a7
AngelStivenToro <torostiven0327@gmail.com>
2026-09-03 12:21:30 -0500

    update to 03/09/2026

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,13 @@ import Main from "@/src/Eliminar/Main";
 export default function Home() {
 	return (
 		<>
+			{/*
+		TODO
+		- Se debe buscar en el proyecto y cambiar todo los CONTAINER_NAME para los despliegues
+		- Buscar los TODO: en el proyecto y revisar si hay algun paso más
+		- Eliminar todo de la carpeta Eliminar en src
+		- Si se instalo Next auth valida que las rutas de api y  middleware esten bien (si existe /login y /logout, ya esta configurado, si no hay que cambiarlos)
+		*/}
 			<Main />
 		</>
 	);
